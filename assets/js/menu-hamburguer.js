5c8d4d6c7a0600dba8416f45501f317ec75bf873
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-03-16 19:10:12 -0300

    Atualizaçao

---
+++ assets/js/menu-hamburguer.js
@@ -0,0 +1,8 @@
+
+const navBar = document.getElementsByClassName('main-nav');
+const menu = document.querySelector("#menu-hamb");
+
+
+menu.addEventListener("click", menuHamb=()=> {
+    navBar.style.display = "flex"
+})
